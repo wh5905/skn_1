649faa982ba364dffcaa57e352d443003577acf5
wonhyeong <jeongwon264@gmail.com>
2024-05-10 17:49:50 +0900

    feat:3일차 마무리

--- project01/runner copy day3.py
+++ project01/runner copy day3.py
@@ -139,3 +139,21 @@ if __name__ == "__main__":
     ]).format(name, weight, height, bmi, result))
 
     
+    #제너레이터
+    def test():
+        print("함수가 호출되었습니다.")
+        yield "test"
+
+
+    print("A 지점 통과")
+    next(test())
+
+    print("B 지점 통과")
+    next(test())
+    print(test())
+
+    # 책의 가격을 기준으로 정렬
+    # book.sort(key = lambda booke : book["가격"])
+
+    
+
